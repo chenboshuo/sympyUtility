87e83742b370792f6a343e4dcc140944774d1561
chenboshuo <chenboshuo.hi@163.com>
2023-06-23 19:06:13 +0800

    :bug:delete wrong called conditions

--- src/sympyUtility.py
+++ src/sympyUtility.py
@@ -1,26 +1,22 @@
 import sympy as sp
 from collections.abc import Iterable
 from IPython.display import display, Math
-from itertools import product
 
-from collections.abc import Iterable
-from IPython.display import display, Math
 
 def show_item(s,latex,cpp,cpp_format):
     if latex:
         print(sp.latex(s))
     if cpp:
         print(cpp_format(sp.printing.cxxcode(s)))
-    display(Math(sp.latex(s)))
+    return display(Math(sp.latex(s)))
 
 
 def show(s,latex=False,cpp=False,cpp_format=lambda *args: args[0]):
-    if __name__ == "__main__" :
-        if isinstance(s, Iterable):
-            for exp in s:
-                show_item(exp,latex,cpp,cpp_format)
-        else:
-            show_item(s,latex,cpp,cpp_format)
+    if isinstance(s, Iterable):
+        for exp in s:
+            return show_item(exp,latex,cpp,cpp_format)
+    else:
+        return show_item(s,latex,cpp,cpp_format)
 
 def bernstein_func(m,i,param):
     return (param**i)*((1-param)**(m-i))*(
